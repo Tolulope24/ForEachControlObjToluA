630d573825683cea90831996edfe3169be06a311
Tolulope24 <tolulope.adebayo@ocsbstudent.ca>
2018-04-06 14:29:35 -0400

    Added Codes

--- ForEachControlObjForm.cs
+++ ForEachControlObjForm.cs
@@ -10,11 +10,43 @@ using System.Windows.Forms;
 
 namespace ForEachControlObjToluA
 {
-    public partial class Form1 : Form
+    public partial class frmForEachControlObj : Form
     {
-        public Form1()
+        public frmForEachControlObj()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Control Button in this. Controls)
+            {
+                Button.BackColor = Color.Crimson;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (Control Button in this.Controls)
+            {
+                Button.BackColor = Color.Gold;
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            foreach (Control Label in this.Controls)
+            {
+                Label.BackColor = Color.Cyan;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (Control RadioButton in this.Controls)
+            {
+               RadioButton.BackColor = Color.Gainsboro;
+            }
         }
     }
 }
